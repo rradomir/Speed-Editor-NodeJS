3abace393e172a1513bb20c02a5ae5b87f0458ce
rradomir <97620566+rradomir@users.noreply.github.com>
2022-01-12 20:18:10 +0100

    Update SpeedEditor.js

--- SpeedEditor.js
+++ SpeedEditor.js
@@ -1,13 +1,11 @@
 /*
 
 based on:
-
 https://github.com/Haavard15/SpeedEditorHID/blob/test/speedEditor.js
 
+authentication formula in python:
 https://github.com/smunaut/blackmagic-misc/blob/master/bmd.py
 # Copyright (C) 2021 Sylvain Munaut <tnt@246tNt.com>
-
-python to node conversion by Radomir Rytych
 
 */
 
@@ -21,10 +19,10 @@ var rol8=function (v){
 }
 
 var rol8n=function(v, n){
-	for (let i=0;i<n;i++) // in range(n):
+	for (let i=0;i<n;i++) 
 		v = rol8(v);
 	return v;
-
+	
 }
 
 var generate=function(challenge){
@@ -52,24 +50,21 @@ var generate=function(challenge){
 	];
 	let MASK = 12077075256910773232n;
 
-  let	n = BigInt( challenge & 7n);
+	let	n = BigInt( challenge & 7n);
 	
-  let v = BigInt(rol8n(challenge, n));
+	let v = BigInt(rol8n(challenge, n));
  
-  let k = BigInt(0n);
+	let k = BigInt(0n);
 
 	if ((v & 1n) == ((120n >> n) & 1n))
 		k = AUTH_EVEN_TBL[n];
 	else
-  {
+	{
 		v = v ^ rol8(v);
 		k = AUTH_ODD_TBL[n];
     }
 	return BigInt(v ^ (rol8(v) & MASK) ^ k);
 }
-
-
-
 
     // Reset the auth state machine
     device.sendFeatureReport(Buffer.from([6,0,0,0,0,0,0,0,0,0]));
@@ -82,11 +77,11 @@ var generate=function(challenge){
     let challenge = data_buf.readBigUInt64LE(2);
 
     // Send our challenge (to authenticate keyboard)
-		// We don't care ... so just send 0x0000000000000000
+	// We don't care ... so just send 0x0000000000000000
     device.sendFeatureReport(Buffer.from([6,1,0,0,0,0,0,0,0,0]));
 
     // Read the keyboard response
-		// Again, we don't care, ignore the result
+	// Again, we don't care, ignore the result
     data=device.getFeatureReport(6, 10);
     if (data[0]!=6||data[1]!=2) console.log("Failed authentication response");
     
@@ -106,103 +101,98 @@ var generate=function(challenge){
 
     
 var buttons = [
-  "Smart Insert",
-  "Append",
-  "Ripple",
-  "Close Up",
-  "Place On Top",
-  "Source",
-  "In",
-  "Out",
-  "Trim In",
-  "Trim Out",
-  "Roll",
-  "Slip Source",
-  "Slip Destination",
-  "Transistion Duration",
-  "Cut",
-  "Dissolve",
-  "Smooth Cut"
+	"Smart Insert",
+	"Append",
+	"Ripple",
+	"Close Up",
+	"Place On Top",
+	"Source",
+	"In",
+	"Out",
+	"Trim In",
+	"Trim Out",
+	"Roll",
+	"Slip Source",
+	"Slip Destination",
+	"Transistion Duration",
+	"Cut",
+	"Dissolve",
+	"Smooth Cut"
 ]
 
 var otherButtons = {
-  49: "Escape",
-  31: "Sync Bin",
-  44: "Audio Level",
-  45: "Full View",
-  34: "Transistion",
-  47: "Split",
-  46: "Snap",
-  43: "Ripple Delete",
-  51: "Camera 1",
-  52: "Camera 2",
-  53: "Camera 3",
-  54: "Camera 4",
-  55: "Camera 5",
-  56: "Camera 6",
-  57: "Camera 7",
-  58: "Camera 8",
-  59: "Camera 9",
-  48: "Live",
-  37: "Video Only",
-  38: "Audio Only",
-  60: "Play / Pause",
-  26: "Source",
-  27: "Timeline",
-  28: "Shuffle",
-  29: "Jog",
-  30: "Scroll"
+	49: "Escape",
+	31: "Sync Bin",
+	44: "Audio Level",
+	45: "Full View",
+	34: "Transistion",
+	47: "Split",
+	46: "Snap",
+	43: "Ripple Delete",
+	51: "Camera 1",
+	52: "Camera 2",
+	53: "Camera 3",
+	54: "Camera 4",
+	55: "Camera 5",
+	56: "Camera 6",
+	57: "Camera 7",
+	58: "Camera 8",
+	59: "Camera 9",
+	48: "Live",
+	37: "Video Only",
+	38: "Audio Only",
+	60: "Play / Pause",
+	26: "Source",
+	27: "Timeline",
+	28: "Shuffle",
+	29: "Jog",
+	30: "Scroll"
 }
 
 Object.keys(otherButtons).forEach(buttonNumber => {
-  buttons[buttonNumber - 1] = otherButtons[buttonNumber];
+	buttons[buttonNumber - 1] = otherButtons[buttonNumber];
 });
 
 var previousButtons = [];
 
 device.on("data", function(data) {
-  console.log(data)
-  var dataArray = [...data];
-  if(dataArray[0] == 4){
-    
-    var buttonsPressed = []
+	console.log(data)
+	var dataArray = [...data];
+	if(dataArray[0] == 4){
+
+	var buttonsPressed = []
     if(dataArray[1] != 0){
-      buttonsPressed.push(dataArray[1])
+		buttonsPressed.push(dataArray[1])
     }
     if(dataArray[3] != 0){
-      buttonsPressed.push(dataArray[3])
+		buttonsPressed.push(dataArray[3])
     }
-    if(dataArray[5] != 0){
-      buttonsPressed.push(dataArray[5])
+	if(dataArray[5] != 0){
+		buttonsPressed.push(dataArray[5])
     }
-    if(dataArray[7] != 0){
-      buttonsPressed.push(dataArray[7])
+	if(dataArray[7] != 0){
+		buttonsPressed.push(dataArray[7])
+	}
+	if(dataArray[9] != 0){
+		buttonsPressed.push(dataArray[9])
+	}
+	if(dataArray[11] != 0){
+		buttonsPressed.push(dataArray[11])
     }
-    if(dataArray[9] != 0){
-      buttonsPressed.push(dataArray[9])
-    }
-    if(dataArray[11] != 0){
-      buttonsPressed.push(dataArray[11])
-    }
-    //console.log(previousButtons)
-    //console.log(buttonsPressed)
-    buttonsPressed.forEach(button => {
-      if(previousButtons.indexOf(button) == -1){
-        console.log("Pressed ", buttons[button - 1])
 
-
-      }
+	buttonsPressed.forEach(button => {
+		if(previousButtons.indexOf(button) == -1){
+			console.log("Pressed ", buttons[button - 1])
+		}
       
     });
-    previousButtons.forEach(button => {
-      if(buttonsPressed.indexOf(button) == -1){
-        console.log("Unpressed ", buttons[button - 1])
-
+	previousButtons.forEach(button => {
+		if(buttonsPressed.indexOf(button) == -1){
+			console.log("Unpressed ", buttons[button - 1])
       }
-      
     });
-    previousButtons = buttonsPressed;
+	previousButtons = buttonsPressed;
 
-  }
+	}
 
 });
