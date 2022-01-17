ef0eb6e0ce8df9286cadaf41019754c891126681
rradomir <97620566+rradomir@users.noreply.github.com>
2022-01-17 14:45:50 +0100

    Added LEDs control

--- SpeedEditor.js
+++ SpeedEditor.js
@@ -1,6 +1,7 @@
 class SpeedEditor {
 	device;
 	keys=[];
+	authTimer;
 	keyNames={
 		1: "Smart Insert",
 		2: "Append",
@@ -44,24 +45,47 @@ class SpeedEditor {
 		27: "Timeline",
 		28: "Shuffle",
 		29: "Jog",
-		30: "Scroll" 
+		30: "Scroll"
 	}
-	
+	static leds={
+		closeUp:	1<<0,
+		cut:		1<<1,
+		dis:		1<<2,
+		smthCut:	1<<3,
+		trans:		1<<4,
+		snap:		1<<5,
+		cam7:		1<<6,
+		cam8:		1<<7,
+		cam9:		1<<8,
+		liveOwr:	1<<9,
+		cam4:		1<<10,
+		cam5:		1<<11,
+		cam6:		1<<12,
+		videoOnly:	1<<13,
+		cam1:		1<<14,
+		cam2:		1<<15,
+		cam3:		1<<16,
+		audioOnly:	1<<17
+	}
+	currentLed=[];
 	keyAction(data){
 	  /* on each keydown or keyup keyboard will send all pressed button at the moment
 	   * so we need to filter new keys and check which keys was released.
 	   * keyboard can handle 6 simultaneous buttons
 	   * */
-	  
+
 		let currentKeys=[];
-		for (let i=0;i<data.length;i+=2) if (data[i]>0) currentKeys.push(data[i]);
-	  
+		for (let i=0;i<data.length;i+=2) {
+			if (data[i]>0) currentKeys.push(data[i]);
+			else break;
+		}
+
 		currentKeys.forEach(button => {
 			if(this.keys.indexOf(button) == -1)
 				this.emit('keydown',{keyCode:button,keyName:this.keyNames[button]});
 		});
-	  
-	  
+
+
 		this.keys.forEach(button => {
 			if(currentKeys.indexOf(button) == -1)
 				this.emit('keyup',{keyCode:button,keyName:this.keyNames[button]});
@@ -76,19 +100,36 @@ class SpeedEditor {
 		if (success) this.device.on("data", (buffer)=>{
 			let data = [...buffer];
 			if (data[0]==4) this.keyAction(data.slice(1));
-			else if (data[0]==3) 
+			else if (data[0]==3)
 				{
-				let value = buffer.readInt32LE(2);
-				this.emit('jog',Math.round(value/360));
+					let value = buffer.readInt32LE(2);
+					this.emit('jog',Math.round(value/360));
 				}
+
 			});
+		this.authTimer=setInterval(()=>{
+			this.authenticate();
+			console.log("reauthenticate");
+			},1500000);
+		this.setLight(0,0);
+
 	}
-	  
-	/* 
+	setLight = (code,value) => {
+		if (value==1&&this.currentLed.indexOf(code)==-1) this.currentLed.push(code);
+		if (value==0&&this.currentLed.indexOf(code)!=-1) this.currentLed.splice(this.currentLed.indexOf(code),1);
+		let leds=0;
+		for (let i of this.currentLed) leds|=i;
+		let buf = Buffer.from([2,0,0,0,0,0]);
+		buf.writeUInt32LE(leds, 1);
+		this.device.write(buf);
+	}
+	
+	
+	/*
 	* Authenticate module is taken from:
 	* https://github.com/smunaut/blackmagic-misc
 	* Copyright (C) 2021 Sylvain Munaut <tnt@246tNt.com>
-	* 
+	*
 	* */
 	rol8(v){
 		return BigInt(((v << 56n) | (v >> 8n)) & 18446744073709551615n)
@@ -99,7 +140,7 @@ class SpeedEditor {
 			v = this.rol8(v);
 		return v;
 	}
-	   
+
 	#AUTH_EVEN_TBL = [
 		4242707987619187656n,
 		3069963097229903046n,
@@ -119,7 +160,7 @@ class SpeedEditor {
 		18266591397768539273n,
 		7035737829027231430n];
 	#MASK = 12077075256910773232n;
-	   
+
 	authenticate(){
 
 	// Reset the auth state machine
@@ -127,12 +168,12 @@ class SpeedEditor {
 
 	// Read the keyboard challenge (for keyboard to authenticate app)
 	let data=this.device.getFeatureReport(6, 10);
-	if (data[0]!=6||data[1]!=0) 
+	if (data[0]!=6||data[1]!=0)
 	{
 		console.log("Failed authentication get challenge");
 		return false;
 	}
-	
+
 	const data_buf=Buffer.from(data);
 	let challenge = data_buf.readBigUInt64LE(2);
 
@@ -143,12 +184,12 @@ class SpeedEditor {
 	// Read the keyboard response
 	// Again, we don't care, ignore the result
 	data=this.device.getFeatureReport(6, 10);
-	if (data[0]!=6||data[1]!=2) 
+	if (data[0]!=6||data[1]!=2)
 	{
 		console.log("Failed authentication response");
 		return false;
 	}
-	
+
 	let	n = BigInt(challenge & 7n);
 	let v = BigInt(this.rol8n(challenge, n));
 	let k = BigInt(0n);
@@ -164,19 +205,19 @@ class SpeedEditor {
 	//Read the status
 	const buf = Buffer.allocUnsafe(8);
 	buf.writeBigUInt64LE(response, 0);
-	
+
 	this.device.sendFeatureReport(Buffer.concat([Buffer.from([6,3]),buf]));
-	
+
 	data=this.device.getFeatureReport(6, 10);
-	if (data[0]!=6||data[1]!=4) 
+	if (data[0]!=6||data[1]!=4)
 		{
 		console.log("Failed authentication status");
 		return false;
 		}
 	return true;
 	}// end of authenticate
-	
+
 }; //end of class
-  
+
 module.exports = SpeedEditor;
 require('util').inherits(SpeedEditor,require('events').EventEmitter);
